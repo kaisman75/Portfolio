57f351942ab024b01ab77d3501584f90ebaba36a
kaisman75 <kaismanai1975gmail.com>
2021-05-23 11:46:14 +0100

    project setup

---
+++ src/Components/hero/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Hero.css"
+import Flip from  'react-reveal/Flip';
+import Bounce from 'react-reveal/Bounce';
+
+//page de presentation
+
+ const Hero = () => {
+    return (
+        <>
+        <div className="heroPage">
+            <div className="hero-name">
+             <Bounce >
+             <h1>Manai Kais</h1>
+             </Bounce>
+            
+            <Flip>
+            <p>Web Devlopper</p>
+           </Flip>
+           </div>
+        </div>
+        </>
+    ) 
+}
+export default Hero
